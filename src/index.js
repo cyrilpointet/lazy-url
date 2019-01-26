1339ce8501ab2e64a865336e4c286f3837d5edbc
cyrilpointet <pointet.cyril@gmail.com>
2019-01-26 21:55:38 +0100

    create lazyUrl class

--- src/index.js
+++ src/index.js
@@ -4,27 +4,63 @@ if (NodeList.prototype.forEach === undefined) {
     NodeList.prototype.forEach = Array.prototype.forEach;
 }
 
-let observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        let element = entry.target;
-        if (entry.intersectionRatio > 0 && element.getAttribute('src') === null) {
-            element.setAttribute('src', element.getAttribute('lazy-url'));
-            observer.unobserve(element);
-        }
-        if (entry.intersectionRatio > 0 && element.getAttribute('src') !== null) {
-            observer.unobserve(element);
-        }
-    })
-}, {
-    threshold: [0.01]
-});
-
-let lazyElements = document.querySelectorAll('[lazy-url]');
-lazyElements.forEach((el) => {
-    if (typeof(el.getAttribute('src')) !== 'string') {
-        if (el.getAttribute('lazy-color')) {
-            el.style.backgroundColor = el.getAttribute('lazy-color');
-        }
-        observer.observe(el);
+class LazyUrl {
+    constructor() {
+        this.observer = new IntersectionObserver(function (entries) {
+            entries.forEach(entry => {
+                let element = entry.target;
+                if (entry.intersectionRatio > 0 && element.getAttribute('src') === null) {
+                    element.setAttribute('src', element.getAttribute('lazy-url'));
+                    element.removeAttribute('lazy-url');
+                    this.unobserve(element);
+                }
+                if (entry.intersectionRatio > 0 && element.getAttribute('src') !== null) {
+                    element.removeAttribute('lazy-url');
+                    this.unobserve(element);
+                }
+            })
+        }, {
+            threshold: [0.01]
+        });
+        this.bodyObserver = new MutationObserver((domChanges) => {
+            this.domChanges(domChanges)
+        });
+        this.bodyObserver.observe(document.body, {
+            subtree: true,
+            childList: true
+        });
     }
-})
+
+    domChanges(domChanges) {
+        let that = this;
+        domChanges.forEach(function (mutation) {
+            mutation.addedNodes.forEach(element => {
+                if (element.nodeType === 1 && element.getAttribute('lazy-url')) {
+                    that.start();
+                }
+            })
+        });
+    }
+
+    start() {
+        this.observer.disconnect();
+        document.querySelectorAll('[lazy-url]').forEach((el) => {
+            if (typeof (el.getAttribute('src')) !== 'string') {
+                if (el.getAttribute('lazy-color')) {
+                    el.style.backgroundColor = el.getAttribute('lazy-color');
+                }
+                this.observer.observe(el);
+            }
+        });
+        console.log('coucou');
+    }
+
+    stop() {
+        this.observer.disconnect();
+    }
+    refresh() {
+        this.observer.start();
+    }
+}
+
+lazyUrl = new LazyUrl();
---
+++ test.js
@@ -0,0 +1,9 @@
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    setTimeout(()=>{
+        let toto = document.createElement('img');
+        toto.setAttribute('lazy-url', 'https://picsum.photos/400/420/?random');
+        toto.classList.add("myImg");
+        document.getElementById('test').appendChild(toto);
+    }, 2000);
+});
