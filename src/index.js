07934eb0c34bb2a8ad1cf44824b071d1f1b5cf7e
cyrilpointet <pointet.cyril@gmail.com>
2019-01-27 14:25:45 +0100

    background image support

--- src/index.js
+++ src/index.js
@@ -5,14 +5,23 @@ if (NodeList.prototype.forEach === undefined) {
 }
 
 class LazyUrl {
-    constructor(attibuteNameImg, attibuteNameColor) {
-        this.attibuteNameImg = typeof(attibuteNameImg) === 'string' ? attibuteNameImg:'lazy-url';
-        this.attibuteNameColor = typeof(attibuteNameColor) === 'string' ? attibuteNameColor:'lazy-color';
-        this.observer = new IntersectionObserver((entries) => {
-            this.intersectionEvent(entries)
+    constructor(options = {}) {
+        this.attibuteNameSrc = options.attibuteNameSrc && typeof(options.attibuteNameSrc) === 'string' ? options.attibuteNameSrc : 'lazy-url';
+        this.attibuteNameBGImage = options.attibuteNameBGImage && typeof(options.attibuteNameBGImage) === 'string' ? options.attibuteNameBGImage : 'lazy-background';
+        this.attibuteNameColor = options.attibuteNameColor && typeof(options.attibuteNameColor) === 'string' ? options.attibuteNameColor : 'lazy-color';
+
+        this.observerSrc = new IntersectionObserver((entries) => {
+            this.intersectionEventSrc(entries)
         }, {
             threshold: [0.01]
         });
+
+        this.observerBGImage = new IntersectionObserver((entries) => {
+            this.intersectionEventBGImage(entries)
+        }, {
+            threshold: [0.01]
+        });
+
         this.bodyObserver = new MutationObserver((domChanges) => {
             this.domChanges(domChanges)
         });
@@ -20,21 +29,35 @@ class LazyUrl {
             subtree: true,
             childList: true
         });
-        console.log(this.attibuteNameImg);
         this.start();
     }
 
-    intersectionEvent(entries) {
+    intersectionEventSrc(entries) {
         entries.forEach(entry => {
             let element = entry.target;
-            if (entry.intersectionRatio > 0 && element.getAttribute('src') === null) {
-                element.setAttribute('src', element.getAttribute(this.attibuteNameImg));
-                element.removeAttribute(this.attibuteNameImg);
-                this.observer.unobserve(element);
+            if (entry.intersectionRatio > 0 && element.getAttribute(this.attibuteNameSrc) !== null && element.getAttribute('src') === null) {
+                element.setAttribute('src', element.getAttribute(this.attibuteNameSrc));
+                element.removeAttribute(this.attibuteNameSrc);
+                this.observerSrc.unobserve(element);
             }
-            if (entry.intersectionRatio > 0 && element.getAttribute('src') !== null) {
-                element.removeAttribute(this.attibuteNameImg);
-                this.observer.unobserve(element);
+            else  if (entry.intersectionRatio > 0 && element.getAttribute('src') !== null) {
+                element.removeAttribute(this.attibuteNameSrc);
+                this.observerSrc.unobserve(element);
+            }
+        })
+    }
+
+    intersectionEventBGImage(entries) {
+        entries.forEach(entry => {
+            let element = entry.target;
+            if (entry.intersectionRatio > 0 && element.getAttribute(this.attibuteNameBGImage) !== null) {
+                element.style.backgroundImage = 'url(' + element.getAttribute(this.attibuteNameBGImage) + ')';
+                element.removeAttribute(this.attibuteNameBGImage);
+                this.observerBGImage.unobserve(element);
+            }
+            else if (entry.intersectionRatio > 0 && element.getAttribute(this.attibuteNameBGImage) === null) {
+                element.removeAttribute(this.attibuteNameSrc);
+                this.observerBGImage.unobserve(element);
             }
         })
     }
@@ -43,7 +66,7 @@ class LazyUrl {
         let that = this;
         domChanges.forEach(function (mutation) {
             mutation.addedNodes.forEach(element => {
-                if (element.nodeType === 1 && element.getAttribute(that.attibuteNameImg)) {
+                if (element.nodeType === 1 && (element.getAttribute(that.attibuteNameSrc) || element.getAttribute(that.attibuteNameBGImage))) {
                     that.start();
                 }
             })
@@ -51,22 +74,34 @@ class LazyUrl {
     }
 
     start() {
-        this.observer.disconnect();
-        document.querySelectorAll(`[${this.attibuteNameImg}]`).forEach((el) => {
+        this.observerSrc.disconnect();
+        this.observerBGImage.disconnect();
+        document.querySelectorAll(`[${this.attibuteNameSrc}]`).forEach((el) => {
             if (typeof (el.getAttribute('src')) !== 'string') {
                 if (el.getAttribute(this.attibuteNameColor)) {
                     el.style.backgroundColor = el.getAttribute(this.attibuteNameColor);
+                    el.removeAttribute(this.attibuteNameColor);
                 }
-                this.observer.observe(el);
+                this.observerSrc.observe(el);
+            }
+        });
+        document.querySelectorAll(`[${this.attibuteNameBGImage}]`).forEach((el) => {
+            if (typeof (el.getAttribute('src')) !== 'string') {
+                if (el.getAttribute(this.attibuteNameBGImage)) {
+                    el.style.backgroundColor = el.getAttribute(this.attibuteNameColor);
+                    el.removeAttribute(this.attibuteNameColor);
+                }
+                this.observerBGImage.observe(el);
             }
         });
     }
 
     stop() {
-        this.observer.disconnect();
+        this.observerSrc.disconnect();
+        this.observerBGImage.disconnect();
     }
     refresh() {
-        this.observer.start();
+        this.observerSrc.start();
     }
 }
 
